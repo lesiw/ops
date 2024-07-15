e280dda35079154dca7f8fbc2c69031c00cb6f65
Chris Lesiw <chris@chrislesiw.com>
2024-07-14 21:22:48 -0400

    Cache built ci binaries

--- cmd/ci/main.go
+++ cmd/ci/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	_ "embed"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
+	"github.com/google/uuid"
 	"lesiw.io/cmdio"
 	"lesiw.io/cmdio/cmd"
 	"lesiw.io/defers"
@@ -51,51 +55,129 @@ func run() error {
 	} else if *install {
 		return installComp()
 	}
-	if err := changeToGitRoot(); err != nil {
+
+	workdir, err := toRootDir()
+	if err != nil {
 		return err
 	}
-	workdir, err := os.Getwd()
+	if err := toCiDir(workdir); err != nil {
+		return err
+	}
+
+	bindir, err := cacheDir("bin")
 	if err != nil {
-		return fmt.Errorf("failed to get working directory: %w", err)
+		return fmt.Errorf("failed to create bincache: %w", err)
 	}
-	if stat, err := os.Stat("ci"); os.IsNotExist(err) || !stat.IsDir() {
-		return fmt.Errorf("no 'ci' directory found: %w", err)
-	}
-	bindir, err := os.MkdirTemp("", "ci")
+
+	mtime, err := newestMtime(".")
 	if err != nil {
-		return fmt.Errorf("failed to create temporary directory: %w", err)
+		return fmt.Errorf("failed to determine src mtime: %w", err)
 	}
-	defers.Add(func() { _ = os.RemoveAll(bindir) })
-	cidir := filepath.Join(workdir, "ci")
-	if err := os.Chdir(cidir); err != nil {
-		return fmt.Errorf("failed to chdir to '%s': %w", cidir, err)
+
+	dirid, err := id()
+	if err != nil {
+		return err
 	}
-	cibin := filepath.Join(bindir, "ci")
-	if err := cmd.Run("go", "build", "-o", cibin, "."); err != nil {
-		return fmt.Errorf("go build failed: %w", err)
+
+	cibin := filepath.Join(bindir, "ci-"+base64.URLEncoding.
+		WithPadding(base64.NoPadding).EncodeToString(dirid[:]))
+	stat, err := os.Stat(cibin)
+	if os.IsNotExist(err) || stat.ModTime().Before(mtime) {
+		if err := cmd.Run("go", "build", "-o", cibin, "."); err != nil {
+			return fmt.Errorf("go build failed: %w", err)
+		}
 	}
+
 	if err := os.Chdir(workdir); err != nil {
 		return fmt.Errorf("failed to chdir to '%s': %w", workdir, err)
 	}
+
 	if err := cmd.Run(append([]string{cibin}, os.Args[1:]...)...); err != nil {
 		return errors.New("")
 	}
 	return nil
 }
 
-func changeToGitRoot() error {
+func id() (id uuid.UUID, err error) {
+	var rawid []byte
+	rawid, err = os.ReadFile(".uuid")
+	var pe *fs.PathError
+	if err == nil {
+		uuidstring := strings.TrimSpace(string(rawid))
+		if id, err = uuid.Parse(uuidstring); err != nil {
+			err = fmt.Errorf("failed to parse .uuid: %s", err)
+			return
+		}
+		return
+	}
+	if !errors.As(err, &pe) {
+		err = fmt.Errorf("failed to read .uuid: %s", err)
+		return
+	}
+	id = uuid.New()
+	err = os.WriteFile(".uuid", []byte(id.String()+"\n"), 0644)
+	if err != nil {
+		err = fmt.Errorf("failed to write .uuid: %s", err)
+		return
+	}
+	return
+}
+
+func cacheDir(path ...string) (cache string, err error) {
+	if cache, err = os.UserCacheDir(); err != nil {
+		return "", fmt.Errorf("failed to get user cache directory: %s", err)
+	}
+	cache = filepath.Join(cache, "ci", filepath.Join(path...))
+	if err = os.MkdirAll(cache, 0755); err != nil {
+		return "", fmt.Errorf("failed to create cache directory: %s", err)
+	}
+	return
+}
+
+func toRootDir() (string, error) {
 	for {
 		cwd, err := os.Getwd()
 		if err != nil {
-			return err
+			return "", err
 		}
 		fileinfo, err := os.Stat(".git")
 		if err == nil && fileinfo.IsDir() {
-			return nil
+			return cwd, nil
 		}
 		reachedRoot := (cwd == "/" || cwd == (filepath.VolumeName(cwd)+"\\"))
 		if reachedRoot || os.Chdir("..") != nil {
-			return fmt.Errorf("No .git directory was found.")
+			return "", fmt.Errorf("No .git directory was found.")
 		}
 	}
+}
+
+func toCiDir(root string) error {
+	if stat, err := os.Stat("ci"); os.IsNotExist(err) || !stat.IsDir() {
+		return fmt.Errorf("no 'ci' directory found: %w", err)
+	}
+	cidir := filepath.Join(root, "ci")
+	if err := os.Chdir(cidir); err != nil {
+		return fmt.Errorf("failed to chdir to '%s': %w", cidir, err)
+	}
+	return nil
+}
+
+func newestMtime(dir string) (mtime time.Time, err error) {
+	err = filepath.Walk(
+		dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			t := info.ModTime()
+			if mtime.IsZero() || t.After(mtime) {
+				mtime = t
+			}
+			return nil
+		},
+	)
+	return
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,10 @@ module lesiw.io/ci
 go 1.22.0
 
 require (
-	gotest.tools v2.2.0+incompatible
+	github.com/google/uuid v1.6.0
 	lesiw.io/cmdio v0.0.0-20240714203209-bedbe3a7fec2
 	lesiw.io/defers v0.7.0
 	lesiw.io/flag v0.7.0
 )
 
-require (
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-)
+require golang.org/x/sync v0.7.0 // indirect
